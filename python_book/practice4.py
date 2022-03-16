20586564b6b209607e97cc24223d6ef0596b0a54
davidjan76324 <david_jan76324@hotmail.com>
2022-03-16 23:36:49 +0800

    @新增List Comprehension ＆ 清單切片(list slice) ＆ 方法鏈結(Method chaining) ＆ 函式鏈結(Function chaining) @增加註解：.sort(reverse=True)：就地排序 ＆ sorted(reverse=True)：複製排序

--- python_book/practice4.py
+++ python_book/practice4.py
@@ -3,24 +3,24 @@ def getFileInfo(fileName, sortType):
         myInfoList = []
         with open(fileName, 'r') as data:
             for info in data:
-                myInfoList = myInfoList + \
-                    list(map(float, info.strip().replace(':', '.').replace('-', '.').split(',')))
+                myInfoList = myInfoList + list(map(float, info.strip().replace(':', '.').replace('-', '.').split(',')))
             print("--- File:{0}".format(fileName))
-            print(mysortList(sortType, myInfoList)['Msg'])
+            print(mysortList(sortType, myInfoList, True)['Msg'])
+            print(sorted(set(mysortList(sortType, myInfoList, True)['Msg']))) #利用set去除重複的&排序
 
     except Exception as ex:
         print("--- Error Message: {0}".format(ex))
 
 
-def mysortList(type: str, mylist: list):
+def mysortList(type: str, mylist: list,reverse:bool):
     try:
 
         if isinstance(mylist, list):
             if type == 'sort':
-                mylist.sort() #破壞性的sort
+                mylist.sort(reverse=reverse) #破壞性的sort (ln-place Sorting, 就地排序)
                 return retunInfo(200, mylist)
             elif type == 'sorted':
-                return retunInfo(200, sorted(mylist)) #非破壞性的sort
+                return retunInfo(200, sorted(mylist,reverse=reverse)) #非破壞性的sort (Copied Sorting, 複製排序)
             else:
                 return retunInfo(200, mylist)
 
@@ -35,3 +35,32 @@ def retunInfo(code: int, msg: str):
 
 getFileInfo('james.txt', 'sort')
 getFileInfo('sarah.txt', 'sort')
+
+
+
+
+#-- List Comprehension
+numbers = listCom = []
+for x in range(10):
+    numbers.append(x * 3)
+print(numbers)
+
+listCom = [x*3 for x in range(10) if x > 4]  # List Comprehension
+print(listCom)
+
+
+#-- list slice 清單切片
+print(listCom[0:3])
+
+
+#-- Method chaining(方法鍊結)
+def showMyList(mylist):
+    return  " ".join(mylist)
+print(showMyList(["fuck","you"]).upper())
+
+#-- function chaining(函式鍊結)
+def showList():
+    return sorted
+
+mmList = [2,22,3,123]
+print(showList()(mmList))
